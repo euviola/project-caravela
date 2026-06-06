6fb3aeee7e72ca2cb08f9c3fe13d8d16fd708c39
euviola <charlzbekker@gmail.com>
2026-06-06 17:08:49 +0300

    Clear GameLoop

--- GameLoop.py
+++
@@ -1,8 +0,0 @@
-from gameobjects_base.gameobject_player.Player import Player
-from gameobjects_base.Entity import Entity
-from enginelayer.EventBus import EventBus
-
-bus = EventBus()
-player = Player(name="Stepan", race="Human", health=100)
-test_entity = Entity(health=100, damage=15)
-
