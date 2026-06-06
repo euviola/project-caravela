b0dc475d37b0712676054801341692464caf76d1
euviola <charlzbekker@gmail.com>
2026-06-06 16:45:08 +0300

    clear the gameloop

--- GameLoop.py
+++ GameLoop.py
@@ -6,9 +6,3 @@ bus = EventBus()
 player = Player(name="Stepan", race="Human", health=100)
 test_entity = Entity(health=100, damage=15)
 
-bus.subscribe("on_entity_attacked_player", test_entity.attack)
-bus.subscribe("on_entity_attacked_player", player.changeHealth)
-
-bus.emit("on_entity_attacked_player", {"Damage": test_entity.damage})
-
-print(player.health)
