153179c8a2419d0cd60e383d094782b3a355b1d6
euviola <charlzbekker@gmail.com>
2026-06-06 17:08:30 +0300

    Update file order & Widen entities list

--- gameobjects_base/Entity.py
+++
@@ -1,7 +0,0 @@
-class Entity:
-    def __init__(self, health, damage):
-        self.health = health
-        self.damage = damage
-
-    def attack(self, data):
-        print(f"Entity attack: {data['Damage']}")
