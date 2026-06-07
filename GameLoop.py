f92b485104c325061f118a4b9ab5ca63b42d5832
euviola <charlzbekker@gmail.com>
2026-06-07 13:54:23 +0300

    clear feature testing in GameLoop

--- GameLoop.py
+++
@@ -1,9 +0,0 @@
-from Engine.CommandParser import CommandParser
-
-cp = CommandParser()
-
-command = "Move to the forest"
-command_split = cp.SplitCommand(command)
-
-print(cp.ReturnFirstArgument(command_split))
-print(cp.ReturnSecondArgument(command_split))
