d143b24a33c4c6207b21d3884602466811300dc4
euviola <charlzbekker@gmail.com>
2026-06-12 16:31:30 +0300

    remake quest.py

--- Quests/Quest.py
+++ Quests/Quest.py
@@ -3,15 +3,49 @@ class Quest:
         self.id = data["id"]
         self.title = data["title"]
         self.stages = data["stages"]
-        self.current_stage = self.stages["start"]
+        self.current_stage_index = 0
         self.completion_conditions = data["completion_conditions"]
         self.status = data["status"]
 
+    @property
+    def current_stage(self):
+        if self.status == "completed":
+            return None
+        return self.stages[self.current_stage_index]
+
+    @property    
+    def objective(self):
+        if self.status == "completed":
+            return None
+        return self.current_stage["description"]
+    
     def start(self):
-        self.status = "Active"
+        self.status = "active"
+        print(f"Новий квест: {self.title}")
+        print(f"{self.objective}")
 
-    def complete(self):
-        self.status = "Complete"
+    def check(self, event, data):
+        if self.status != "active":
+            return
+        
+        condition = self.current_stage["completion_condition"]
 
-    def is_active(self) -> bool:
-        return self.status == "Active"
+        if event != condition["event"]:
+            return
+        
+        for key, value in condition.items():
+            if key == "event":
+                continue
+            if data.get(key) != value:
+                return
+        
+        self.advance()
+
+    def advance(self):
+        self.current_stage_index += 1
+
+        if self.current_stage_index >= len(self.stages):
+            self.status = "completed"
+            print(f"Quest: {self.title} is completed")
+        else:
+            print(f"{self.objective}")
--- Quests/QuestTracker.py
+++ Quests/QuestTracker.py
@@ -8,16 +8,3 @@ class QuestTracker:
 
     def add(self, quest):
         self.quests[quest.id] = quest
-
-    def on_npc_talked(self, data):
-        if data["npc_id"] == "demo_npc_1" and "demo_quest_1" in self.quests:
-            quest = self.quests["testquest"]
-            if quest.status == "Inactive":
-                quest.start()
-
-    def on_killed_rat(self, data):
-        kill_counter = 0
-        if data["entity_id"] == ["demo_rat"] and "demo_quest_1" in self.quests:
-            kill_counter += 1
-            if kill_counter == 3:
-                
